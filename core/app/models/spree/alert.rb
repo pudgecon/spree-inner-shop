71af32fadbf05107359fa0c3ad66af32bfe7ac30
Pudgecon <pudgecon@gmail.com>
2014-12-30 22:00:44 +0800

    Not check for updates

--- core/app/models/spree/alert.rb
+++ core/app/models/spree/alert.rb
@@ -3,15 +3,15 @@ require 'httparty'
 module Spree
   class Alert
     def self.current(host)
-      params = {
-        version: Spree.version,
-        name: Spree::Config[:site_name],
-        host: host,
-        rails_env: Rails.env,
-        rails_version: Rails.version
-      }
+      #params = {
+        #version: Spree.version,
+        #name: Spree::Config[:site_name],
+        #host: host,
+        #rails_env: Rails.env,
+        #rails_version: Rails.version
+      #}
 
-      HTTParty.get('http://alerts.spreecommerce.com/alerts.json', query: params).parsed_response
+      #HTTParty.get('http://alerts.spreecommerce.com/alerts.json', query: params).parsed_response
     end
   end
 end
